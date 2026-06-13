97eb6def2b99fce75a249938746b3f518beefcc9
Franco Ñaña <franconana3@gmail.com>
2026-06-13 13:16:53 -0300

    Configuración Para el Video

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,11 +3,18 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.enableCors({
-    origin: 'http://localhost:4200',
+    origin: [
+      'http://localhost',
+      'https://localhost',
+      'http://localhost:4200'
+    ],
     methods: 'GET,POST,PATCH,DELETE',
     allowedHeaders: 'Content-Type,Authorization',
   });
+
   await app.listen(3000);
 }
+
 bootstrap();
